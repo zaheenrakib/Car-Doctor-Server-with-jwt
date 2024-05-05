db198b286469475e1fad83fa30e42271493ca265
zaheenrakib <rakibislambest@gmail.com>
2024-05-05 12:14:10 +0600

    Add Booking

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -29,12 +29,40 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serviceCollection = client.db('carDoctor').collection('services');
+    const bookingCollection = client.db('carDoctor').collection('bookings')
+
+    app.get('/services', async(req,res) =>{
+        const cursor = serviceCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/services/:id', async (req,res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { title: 1, price: 1, service_id:1 },
+          };
+
+        const result = await serviceCollection.findOne(query , options);
+        res.send(result);
+    })
+
+    // booking
+    app.post('/bookings',async(req,res) => {
+      const booking = req.body;
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
